41942b2e7be318fc8c549582ecd004f40ceeeaf6
vdzk <vsevolod.zubov@gmail.com>
2020-10-26 16:28:44 +0200

    Adjust canvas dimension to device pixel ratio

--- public/js/layout.js
+++ public/js/layout.js
@@ -8,10 +8,12 @@ const update = () => {
   const size = Math.min(...['height', 'width'].map(dim => 
     parseInt(parentStyles.getPropertyValue(dim))
   ))
+  const dpr = window.devicePixelRatio || 1
 
-
-  canvasEl.height = size
-  canvasEl.width = size
+  canvasEl.height = size * dpr
+  canvasEl.width = size * dpr
+  canvasEl.style.height = size + 'px'
+  canvasEl.style.width = size + 'px'
   appEl.style.opacity = 1
   updateCallbacks.forEach(cb => cb())
 }
